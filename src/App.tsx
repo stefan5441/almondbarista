a2693ec3f8cfdf55358500afe6d9792243316239
stefan5441 <stefan.cambov2000@gmail.com>
2025-12-25 16:11:43 +0100

    Basic skeleton setup and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState<string>("");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="box">
+        <h1>What's on your mind today?</h1>
+
+        <textarea value={text} onChange={(e) => setText(e.target.value)} className="textbox" rows={12} />
+
+        <div className="footer">
+          <button className="btn">Submit</button>
+          <div>
+            <button className="btn">-</button>
+            <span className="noteNumber">3</span>
+            <button className="btn">+</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
